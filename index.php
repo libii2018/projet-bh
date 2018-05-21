4b11b502201fb0cc417a7052f0a733f3ee60cf71
jojo <libiijoseph@gmail.com>
2018-05-22 00:15:10 +0100

    version 1

--- index.php
+++ index.php
@@ -1,66 +1,123 @@
-<!DOCTYPE html>
 <?php
-        $serveur = "localhost";
-        $login = "root";
-        $pass = "";
 
-       
-        $connexion = new PDO('mysql:host=$serveur;bdname=client_bh',$login, $pass);
-        
-              if(!empty($_POST['pseudo']) AND !empty($_POST['mot_de_pass']) AND !empty($_POST['mot_de_pass2']) )
+$bdd = new PDO('mysql:host=localhost;dbname=espace-membre', 'root', '');
+
+if(isset($_POST['forminscription']))
+{
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $mail = htmlspecialchars($_POST['mail']);
+        $mail2 = htmlspecialchars($_POST['mail2']);
+        $mdp = sha1($_POST['mdp']);
+        $mdp2 = sha1($_POST['mdp2']);
+
+    if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND!empty($_POST['mdp']) AND!empty($_POST['mdp2']))
+    {
+        $pseudolength = strlen($peudo);
+        if($pseudolength <= 255)
+        {
+            If($mail == $mail2)
             {
-                echo "ok";
+                if($mdp == $mdp2)
+                {
+                    $insertmbr =  $bdd->prepare("INSERT INTO membre(pseudo, mail, motdepasse) VALUES(?,?,?)");
+                    $insertmbr ->execute(array($pseudo,$mail,$mdp));
+                    $erreur = "Votre compte a ete cree";
+                }
+                else
+                {
+                    $erreur = "Vos deux mot de passe ne correspondent pas";
+                }
             }
             else
             {
-                echo "non"
+                $erreur="Vos deux adresses mail ne correspondent pas";
             }
-    ?>
+        }
+        else
+        {
+            $erreur = "Votre pseudo ne doit pas depasse 255 carateres";
+        }
+    }
+    else
+    {
+        $erreur = "Tout les champs doivent etre complete";
+    }
+}
+
+?>
+
+
+
+
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title>TUTO PHP</title>
-</head>
+    <head>
+        <title>formulaire</title>
+        <meta charset="utf-8">
+    </head>
     <body>
-     
         <div align="center">
-            <h2>Inscription</h2>
-            <br/><br/>
-            <form  action="" method="post">
+            <h2>Inscription</h2><br/><br/><br/>
+            <form method="POST" action="">
                 <table>
-                    <tr>
-                        <td align="right">
-                            <label for="pseudo">Pseudo :</label>
+                    <tr>    
+                        <td align=right>
+                            <label for="pseudo">Pseudo:</label>
                         </td>
                         <td>
-                            <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" />
+                            <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) {echo $pseudo;}?>"/>
                         </td>
                     </tr>
-                    <tr>
-                        <td align="right">
-                            <label for="mot_de_pass">Mot de passe :</label>
+
+                    <tr>    
+                        <td align=right>
+                            <label for="mail">mail:</label>
                         </td>
                         <td>
-                            <input type="text" placeholder="Mot de passe" id="mot_de_pass" name="mot_de_pass" />
+                            <input type="mail" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) {echo $mail;}?>"/>
                         </td>
                     </tr>
-                    <tr>
-                        <td align="right">
-                            <label for="mot_de_pass2">confirmation votre mot de passe :</label>
+
+                    <tr>    
+                        <td align=right>
+                            <label for="mail2">Confirmation du mail:</label>
                         </td>
-                        <td> 
-                            <input type="text" placeholder="Mot de passe" id="mot_de_pass2" name="mot_de_pass2" />
+                        <td>
+                            <input type="mail" placeholder="Confirmation du mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) {echo $mail2;}?>"/>
                         </td>
                     </tr>
-                    <tr>
+
+                    <tr>    
+                        <td align=right>
+                            <label for="mdp">Votre mot de passe</label>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Votre mot de passe" id="mdp" name="mdp"/>
+                        </td>
+                    </tr>
+
+                    <tr>    
+                        <td align=right>
+                            <label for="mdp2">Confimation de votre mot de passe</label>
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Confimation de votre mot de passe" id="mdp2" name="mdp2"/>
+                        </td>
+                    </tr>
+
+                    <tr align="center">
                         <td></td>
                         <td>
                             <br/>
-                            <input type="submit" value="Je m'inscrit" name="forminscription"/>
+                            <input type="submit" name="forminscription" value="je m'inscris" />
                         </td>
                     </tr>
-                </table>
-                
+                </table>       
             </form>
+            <?php
+                if(isset($erreur)){
+                    echo '<font color="red">'. $erreur.'</font>';
+                }
+            ?>
+        </div>
     </body>
 </html>
